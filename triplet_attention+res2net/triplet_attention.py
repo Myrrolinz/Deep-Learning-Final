0b8183f2fd5d56a3daaf11656c2c8d8e625385cf
kjy <2979267378@qq.com>
2023-05-23 11:21:45 +0800

    搭建注意力模块

--- triplet_attention+res2net/triplet_attention.py
+++ triplet_attention+res2net/triplet_attention.py
@@ -10,3 +10,26 @@ class ZPool(nn.Module):
         return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
 
 
+# 注意力模块
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        # 先Z-Pool池化
+        self.z_pool = ZPool()
+        # 再卷积 k*k的卷积核卷积->归一化处理
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3),
+            nn.BatchNorm2d(1)
+        )
+        # 通过sigmoid来生成的注意力权值
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        z_pool_out = self.z_pool(x)
+        out = self.conv(z_pool_out)
+        return x * self.sigmoid(out)
+
+
+
+
+
