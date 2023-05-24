2684b79733457fa0cf3158599429063f786724a1
kjy <2979267378@qq.com>
2023-05-24 09:42:36 +0800

    triplet_attention网络搭建完毕并测试

--- triplet_attention+res2net/triplet_attention.py
+++ triplet_attention+res2net/triplet_attention.py
@@ -30,6 +30,44 @@ class SpatialGate(nn.Module):
         return x * self.sigmoid(out)
 
 
+class TripletAttention(nn.Module):
+    def __init__(self, spatial=True):
+        super(TripletAttention, self).__init__()
+        self.spatial = spatial
+
+        # 对三个分支进行初始化
+        # 通道C和空间W维度交互
+        self.cw = SpatialGate()
+        # 通道C和空间H维度交互
+        self.ch = SpatialGate()
+        # 空间H和空间W进行注意力计算
+        if self.spatial:
+            self.hw = SpatialGate()
+
+    def forward(self, x):
+        # 0 2 1 3    b w c h
+        x_perm1 = x.permute(0, 2, 1, 3).contiguous()
+        x_out1 = self.ch(x_perm1)
+        # 0 2 1 3    b c w h
+        x_out1 = x_out1.permute(0, 2, 1, 3).contiguous()
+
+        # 0 3 2 1    b h w c
+        x_perm2 = x.permute(0, 3, 2, 1).contiguous()
+        x_out2 = self.cw(x_perm2)
+        # 0 3 2 1    b c w h
+        x_out2 = x_out2.permute(0, 3, 2, 1).contiguous()
+
+        if self.spatial:
+            x_out3 = self.hw(x)
+            return (1 / 3) * (x_out1 + x_out2 + x_out3)
+        else:
+            return (1 / 2) * (x_out1 + x_out2)
 
 
+if __name__ == '__main__':
+    model = TripletAttention()
+    print(model)
 
+    random_input = torch.randn(1, 16, 256, 256)
+    out = model(random_input)
+    print(out.shape)
