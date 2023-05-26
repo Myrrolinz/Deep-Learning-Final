b39fb9951fccc70e3656115c520070fa026dfe61
@civilizwa <2243987073@qq.com>
2023-05-26 16:29:46 +0800

    finish triplet+resnet train

--- tripletnet.py
+++ tripletnet.py
@@ -2,64 +2,96 @@ import torch
 import torch.nn as nn
 
 
-# z-pool池化层
-class ZPool(nn.Module):
+class BasicConv(nn.Module):
+    def __init__(
+        self,
+        in_planes,
+        out_planes,
+        kernel_size,
+        stride=1,
+        padding=0,
+        dilation=1,
+        groups=1,
+        relu=True,
+        bn=True,
+        bias=False,
+    ):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(
+            in_planes,
+            out_planes,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias,
+        )
+        self.bn = (
+            nn.BatchNorm2d(out_planes, eps=1e-5, momentum=0.01, affine=True)
+            if bn
+            else None
+        )
+        self.relu = nn.ReLU() if relu else None
+
     def forward(self, x):
-        # Z-pool=[MaxPool,AvgPool]
-        # torch.cat的使用:https://blog.csdn.net/xinjieyuan/article/details/105208352
-        return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
 
 
-# 注意力模块
+class ChannelPool(nn.Module):
+    def forward(self, x):
+        return torch.cat(
+            (torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1
+        )
+
+
 class SpatialGate(nn.Module):
     def __init__(self):
         super(SpatialGate, self).__init__()
-        # 先Z-Pool池化
-        self.z_pool = ZPool()
-        # 再卷积 k*k的卷积核卷积->归一化处理
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3),
-            nn.BatchNorm2d(1)
+        kernel_size = 7
+        self.compress = ChannelPool()
+        self.spatial = BasicConv(
+            2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False
         )
-        # 通过sigmoid来生成的注意力权值
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        z_pool_out = self.z_pool(x)
-        out = self.conv(z_pool_out)
-        return x * self.sigmoid(out)
+        x_compress = self.compress(x)
+        x_out = self.spatial(x_compress)
+        scale = torch.sigmoid_(x_out)
+        return x * scale
 
 
 class TripletAttention(nn.Module):
-    def __init__(self, spatial=True):
+    def __init__(
+        self,
+        gate_channels,
+        reduction_ratio=16,
+        pool_types=["avg", "max"],
+        no_spatial=False,
+    ):
         super(TripletAttention, self).__init__()
-        self.spatial = spatial
-
-        # 对三个分支进行初始化
-        # 通道C和空间W维度交互
-        self.cw = SpatialGate()
-        # 通道C和空间H维度交互
-        self.ch = SpatialGate()
-        # 空间H和空间W进行注意力计算
-        if self.spatial:
-            self.hw = SpatialGate()
+        self.ChannelGateH = SpatialGate()
+        self.ChannelGateW = SpatialGate()
+        self.no_spatial = no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate()
 
     def forward(self, x):
-        # 0 2 1 3    b w c h
         x_perm1 = x.permute(0, 2, 1, 3).contiguous()
-        x_out1 = self.ch(x_perm1)
-        # 0 2 1 3    b c w h
-        x_out1 = x_out1.permute(0, 2, 1, 3).contiguous()
-
-        # 0 3 2 1    b h w c
+        x_out1 = self.ChannelGateH(x_perm1)
+        x_out11 = x_out1.permute(0, 2, 1, 3).contiguous()
         x_perm2 = x.permute(0, 3, 2, 1).contiguous()
-        x_out2 = self.cw(x_perm2)
-        # 0 3 2 1    b c w h
-        x_out2 = x_out2.permute(0, 3, 2, 1).contiguous()
-
-        if self.spatial:
-            x_out3 = self.hw(x)
-            return (1 / 3) * (x_out1 + x_out2 + x_out3)
+        x_out2 = self.ChannelGateW(x_perm2)
+        x_out21 = x_out2.permute(0, 3, 2, 1).contiguous()
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x)
+            x_out = (1 / 3) * (x_out + x_out11 + x_out21)
         else:
-            return (1 / 2) * (x_out1 + x_out2)
-
+            x_out = (1 / 2) * (x_out11 + x_out21)
+        return x_out
