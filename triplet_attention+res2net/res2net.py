bedfa3601ed0bef44d682ef5dd4fb486e7d3e003
kjy <2979267378@qq.com>
2023-06-22 09:41:35 +0800

    add se module

--- triplet_attention+res2net/res2net.py
+++ triplet_attention+res2net/res2net.py
@@ -1,8 +1,24 @@
 import torch
 import torch.nn as nn
 
-# # SE模块要添加吗
-# class SEModule(nn.Module):
+
+#SE模块
+class SEModule(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(SEModule, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Conv2d(channels, channels // reduction, kernel_size=1, padding=0)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Conv2d(channels // reduction, channels, kernel_size=1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input):
+        x = self.avg_pool(input)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return input * x
 
 
 # 残差层
@@ -11,7 +27,7 @@ class Bottleneck(nn.Module):
     expansion = 4
 
     # 后续需要为其添加新参数
-    def __init__(self, inplanes, planes, stride=1, downsample=None, scales=4, groups=1, is_first_block=0):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, scales=4, groups=1, is_first_block=0, se=True):
         super(Bottleneck, self).__init__()
 
         self.downsample = downsample
@@ -36,12 +52,13 @@ class Bottleneck(nn.Module):
         self.conv3 = nn.Conv2d(outplanes, planes * self.expansion, kernel_size=1, stride=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * self.expansion)
 
-        # self.relu = nn.ReLU(inplace=True)
-        self.celu = nn.CELU(inplace=True)
+        self.relu = nn.ReLU(inplace=True)
         # 处理第一个块
         if is_first_block == 1:
             self.pool = nn.AvgPool2d(kernel_size=3, stride=stride, padding=1)
 
+        # SE模块
+        self.se = SEModule(planes * self.expansion) if se else None
 
     def forward(self, x):
         identity = x # 将原始输入暂存为shortcut的输出
@@ -53,7 +70,7 @@ class Bottleneck(nn.Module):
         # 1*1卷积层
         out = self.conv1(x)
         out = self.bn1(out)
-        out = self.celu(out)
+        out = self.relu(out)
 
         # 3*3卷积结构
         # x_scale = torch.chunk(out, self.scales, 1)  # 将x分割成scales块
@@ -81,7 +98,7 @@ class Bottleneck(nn.Module):
             else:
                 y_scale = y_scale + x_scales[i]
             y_scale = self.conv2[i](y_scale)
-            y_scale = self.celu(self.bn2[i](y_scale))
+            y_scale = self.relu(self.bn2[i](y_scale))
             if i == 0:
                 out = y_scale
             else:
@@ -96,18 +113,20 @@ class Bottleneck(nn.Module):
         out = self.bn3(out)
 
         # 是否加入SE模块--该结构利用系数scale来使网络自适应的减弱或增强该通道的特征，与注意力机制有异曲同工之妙。
+        if self.se is not None:
+            out = self.se(out)
 
         # 添加triplet_attention
 
         # 残差连接 out=F(X)+X
         out += identity
-        out = self.celu(out)
+        out = self.relu(out)
 
         return out
 
 
 class Res2Net(nn.Module):
-    def __init__(self, block, layers, num_classes=1000, scales=4, groups=1):
+    def __init__(self, block, layers, num_classes=1000, scales=4, groups=1, se=True):
         super(Res2Net, self).__init__()
         # 通道数初始化
         self.inplanes = 64
@@ -115,15 +134,14 @@ class Res2Net(nn.Module):
         # 起始：7*7的卷积层，3*3的最大池化层
         self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(self.inplanes)
-        # self.relu = nn.ReLU(inplace=True)
-        self.celu = nn.CELU(inplace=True)
+        self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         # 残差结构
-        self.layer1 = self._make_layer(block, 64, layers[0], stride=1, scales=scales, groups=groups)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, scales=scales, groups=groups)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, scales=scales, groups=groups)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, scales=scales, groups=groups)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1, scales=scales, groups=groups, se=se)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, scales=scales, groups=groups, se=se)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, scales=scales, groups=groups, se=se)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, scales=scales, groups=groups, se=se)
 
         # 平均池化+全连接层
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
@@ -132,14 +150,13 @@ class Res2Net(nn.Module):
         # 权重初始化
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                # nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
         # 为啥还初始化了bn????
 
-    def _make_layer(self, block, planes, layer, stride=1, scales=4, groups=1):
+    def _make_layer(self, block, planes, layer, stride=1, scales=4, groups=1, se=True):
         # 积步长不为1或深度扩张有变化，导致F(X)与X的shape不同的残差块，就要对X定义下采样函数，使之shape相同
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
@@ -151,19 +168,19 @@ class Res2Net(nn.Module):
         layers = []
         # 第一个残差块需要下采样  def __init__(self, inplanes, planes, stride=1, downsample=None, scales=4, groups=1):
         layers.append(block(self.inplanes, planes, stride=stride, downsample=downsample,
-                            scales=scales, groups=groups, is_first_block=1))
+                            scales=scales, groups=groups, is_first_block=1, se=se))
         self.inplanes = planes * block.expansion
 
         # 通过循环堆叠其余残差块
         for i in range(1, layer):
-            layers.append(block(self.inplanes, planes, scales=scales, groups=groups))
+            layers.append(block(self.inplanes, planes, scales=scales, groups=groups, se=se))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
-        x = self.celu(x)
+        x = self.relu(x)
         x = self.maxpool(x)
 
         x = self.layer1(x)
@@ -180,8 +197,8 @@ class Res2Net(nn.Module):
 
 
 # def __init__(self, block, layers, num_classes=1000, scales=4, groups=1):
-def res2net50(num_classes=1000, scales=4, groups=1):
-    return Res2Net(Bottleneck, [3, 4, 6, 3], num_classes, scales, groups)
+def res2net50(num_classes=1000, scales=4, groups=1, se=True):
+    return Res2Net(Bottleneck, [3, 4, 6, 3], num_classes, scales, groups, se)
 
 
 def res2net101(num_classes=1000, scales=4, groups=1):
