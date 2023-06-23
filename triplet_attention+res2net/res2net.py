17840768b50bcee4648d233ff0e14d875143f63e
Jiazhen-Song <101553029+Jiazhen-Song@users.noreply.github.com>
2023-06-23 15:35:48 +0800

    triplet

--- triplet_attention+res2net/res2net.py
+++ triplet_attention+res2net/res2net.py
@@ -1,6 +1,68 @@
 import torch
 import torch.nn as nn
 
+#Triplet_attention
+
+# z-pool池化层
+class ZPool(nn.Module):
+    def forward(self, x):
+        # Z-pool=[MaxPool,AvgPool]
+        # torch.cat的使用:https://blog.csdn.net/xinjieyuan/article/details/105208352
+        return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
+
+
+# 注意力模块
+class SpatialGate(nn.Module):
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        # 先Z-Pool池化
+        self.z_pool = ZPool()
+        # 再卷积 k*k的卷积核卷积->归一化处理
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7, stride=1, padding=3),
+            nn.BatchNorm2d(1)
+        )
+        # 通过sigmoid来生成的注意力权值
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        z_pool_out = self.z_pool(x)
+        out = self.conv(z_pool_out)
+        return x * self.sigmoid(out)
+
+
+class TripletAttention(nn.Module):
+    def __init__(self, spatial=True):
+        super(TripletAttention, self).__init__()
+        self.spatial = spatial
+
+        # 对三个分支进行初始化
+        # 通道C和空间W维度交互
+        self.cw = SpatialGate()
+        # 通道C和空间H维度交互
+        self.ch = SpatialGate()
+        # 空间H和空间W进行注意力计算
+        if self.spatial:
+            self.hw = SpatialGate()
+
+    def forward(self, x):
+        # 0 2 1 3    b w c h
+        x_perm1 = x.permute(0, 2, 1, 3).contiguous()
+        x_out1 = self.ch(x_perm1)
+        # 0 2 1 3    b c w h
+        x_out1 = x_out1.permute(0, 2, 1, 3).contiguous()
+
+        # 0 3 2 1    b h w c
+        x_perm2 = x.permute(0, 3, 2, 1).contiguous()
+        x_out2 = self.cw(x_perm2)
+        # 0 3 2 1    b c w h
+        x_out2 = x_out2.permute(0, 3, 2, 1).contiguous()
+
+        if self.spatial:
+            x_out3 = self.hw(x)
+            return (1 / 3) * (x_out1 + x_out2 + x_out3)
+        else:
+            return (1 / 2) * (x_out1 + x_out2)
 
 #SE模块
 class SEModule(nn.Module):
@@ -88,6 +150,9 @@ class Bottleneck(nn.Module):
         self.se = SEModule(planes * self.expansion) if se else None
         #se111
         self.semodule = SEModule(outplanes // scales)
+        
+        #Triplet模块
+        self.triplet_attention = TripletAttention()
 
     def forward(self, x):
         identity = x # 将原始输入暂存为shortcut的输出
@@ -146,10 +211,9 @@ class Bottleneck(nn.Module):
             y_scale = self.conv2[i](y_scale)
             
             #se111
-            y_scale = self.semodule(y_scale)
-
+            #y_scale = self.semodule(y_scale)
             y_scale = self.activation(self.bn2[i](y_scale))
-
+            # y_scale = self.triplet_attention(y_scale)
             
 
             if i == 0:
@@ -168,7 +232,7 @@ class Bottleneck(nn.Module):
         # 是否加入SE模块--该结构利用系数scale来使网络自适应的减弱或增强该通道的特征，与注意力机制有异曲同工之妙。
         # if self.se is not None:
         #     out = self.se(out)
-
+        out = self.triplet_attention(out)
         # 添加triplet_attention
 
         # 残差连接 out=F(X)+X
@@ -246,21 +310,21 @@ class Res2Net(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
+                 nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                 nn.BatchNorm2d(planes * block.expansion),
+             )
 
         layers = []
         # 第一个残差块需要下采样  def __init__(self, inplanes, planes, stride=1, downsample=None, scales=4, groups=1):
         layers.append(block(self.inplanes, planes, stride=stride, downsample=downsample,
                             scales=scales, groups=groups, is_first_block=1, se=se))
         self.inplanes = planes * block.expansion
-        self.avgpool = nn.AvgPool2d(kernel_size=2,stride=2)
+        # self.avgpool = nn.AvgPool2d(kernel_size=2,stride=2)
 
         # 通过循环堆叠其余残差块
         for i in range(1, layer):
             layers.append(block(self.inplanes, planes, scales=scales, groups=groups, se=se))
-
+            
         return nn.Sequential(*layers)
 
     def forward(self, x):
